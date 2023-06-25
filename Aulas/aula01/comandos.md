e370c84c6a51e0070d1d6ad3974d6c16935873bc
ViniciusAlvesAraujo <vinicius.araujo1239@gmail.com>
2023-06-25 11:46:59 -0300

    Adicionando aula 01

---
+++ Aulas/aula01/comandos.md
@@ -0,0 +1,6 @@
+# Comandos usados na aula
+
+##
+
+  -ng new aula-01 - cria um novo projeto Angular
+  -ng generate component /views/novo-component - gera um novo component
---
+++ Aulas/aula01/src/app/views/login/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "../home/home.component";
+import { LoginComponent } from "./login.component";
+import { CadastroComponent } from "../cadastro/cadastro.component";
+
+const routes: Routes = [
+  { path: "", component: HomeComponent},
+  { path: "login", component: LoginComponent},
+  { path: "cadastro", component: CadastroComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule{
+
+}
