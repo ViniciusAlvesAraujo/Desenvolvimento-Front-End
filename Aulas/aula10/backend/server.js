969c1b5de478b677c29ea06ef17b6f440ca17bf1
ViniciusAlvesAraujo <vinicius.araujo1239@gmail.com>
2023-08-25 19:34:44 -0300

    Configurando banco de dados e criando server.js

---
+++ Aulas/aula10/backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 8050
+const connectionString = "mongodb+srv://admin:admin123@appdatabase.ry0gm2w.mongodb.net/"
+
+mongoose.connect(connectionString, {
+    dbName: "DevsMarket"
+}).then(() => {
+    console.log("MongoDB UP!")
+    console.log(`Listening on http://localhost:${port}`)
+    app.listen()
+}).catch((error) => {
+    console.log(error)
+})
